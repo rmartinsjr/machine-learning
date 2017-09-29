60c9950c6546a2875ef80474b7c37ecfd8f312fd
Roberto Martins <rmartinsjr@gmail.com>
2017-09-29 20:00:12 -0300

    Completed mini-project
    PCA mini-project completed

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -66,7 +66,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+#n_components = 150
+n_components = 250
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -75,6 +76,7 @@ print "done in %0.3fs" % (time() - t0)
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
+print "Explained variance: ", pca.explained_variance_ratio_[0], pca.explained_variance_ratio_[1]
 print "Projecting the input data on the eigenfaces orthonormal basis"
 t0 = time()
 X_train_pca = pca.transform(X_train)
